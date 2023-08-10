d3b32499a22a48115d4ab430b310e7b0a919115d
anubhav5079 <anubhav.k@outlook.com>
2023-08-11 00:29:36 +0530

    fix cors error

--- server/index.js
+++ server/index.js
@@ -8,62 +8,77 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth.js"
-import userRoutes from "./routes/users.js"
-import postRoutes from "./routes/posts.js"
-import { register } from  "./controllers/auth.js";
-import {createPost } from  "./controllers/posts.js"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
-import { users,posts } from "./data/index.js"
+import { users, posts } from "./data/index.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+
+// Handle CORS Error by adding a header here
+app.use((_, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}))
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json( {limit : "30mb", extended: true }));
-app.use(bodyParser.urlencoded({limit : "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')))
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // FILE STORAGE
 const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-       cb(null, "public/assets");
-    },
-    filename: function(req, file, cb){
-       cb(null, file.originalname);
-    }
-}) ;
-const upload = multer({storage});
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
 //ROUTES WITH FILES
 
 app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts", verifyToken,upload.single("picture"),createPost);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 //Routes
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 //MONGOOSE SETUP
-const PORT= process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL,{
-  useNewUrlParser: true, 
-  useUnifiedTopology: true,
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-}).then(() => {
-  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-  // // ADD DATA ON THE WEBSTIE
-  // User.insertMany(users);
-  // Post.insertMany(posts);
-})
-.catch((error) => console.log(`${error} did not connect`));
-
+    // // ADD DATA ON THE WEBSTIE
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
